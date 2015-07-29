ed712fcc31835f0bd063e4be063fded21f181f5e
Eli <emoretafeliz@synthesio.com>
2015-07-29 18:20:21 -0400

    added more grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,12 +18,25 @@ module.exports = function(grunt) {
 				banner: '/*! <%= pkg.name %>' + ' <%= grunt.template.today("yyyy-mm-dd") %>*/\n'
 			},
 			dist: {
-				src: 'memory.js',
-				dest: 'memory.min.js'
+				src: 'memory.js', //<%= pkg.name %>
+				dest: 'memory.min.js' //<%= pkg.name %><%= pkg.version %>
+			}
+		},
+		cssmin: {
+			compress: {
+				options: {
+					banner: '<% /*! <%= pkg.name %>' + ' <%= grunt.template.today("yyyy-mm-dd") %>*/ %>'
+				},
+				files: {
+					'style.min.css': ['reset.css', 'style.css']
+				}
 			}
 		}
 	}); 
+
+	//load tasks
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['uglify', 'cssmin']);
 };
