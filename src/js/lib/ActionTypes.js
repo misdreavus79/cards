6f8b5dc12c8f4db183dd727c49892b8ccc81279f
Eliezer Moreta-Feliz <eum113@gmail.com>
2017-06-26 10:32:45 -0400

    added Flux pattern files

---
+++ src/js/lib/ActionTypes.js
@@ -0,0 +1,13 @@
+const ActionTypes = {
+	start: 'START_GAME',
+	end: 'END_GAME',
+	usePowerup: 'USE_POWERUP',
+	match: 'MATCH_TILES',
+	win: 'LEVEL_WIN',
+	lose: 'LEVEL_LOSE',
+	fillPowerbar: 'FILL_POWER_BAR',
+	respawn: 'RESPAWN_TILES',
+	shuffle: 'SHUFFLE_BOARD',
+};
+
+export default ActionTypes;
